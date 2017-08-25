d7ea45f38b51ac1e0082d0a536aa5c2abc6de4bc
Nardberjean <Nardberjean@users.noreply.github.com>
2017-08-25 19:09:30 -0400

    Create IndexController.php

---
+++ IndexController.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the Symfony-Util package.
+ *
+ * (c) Jean-Bernard Addor
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SymfonyUtil\Controller\TemplatingFoundationModel;
+
+use PhpObjectInterfaces\Model\TraversableListInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Templating\EngineInterface;
+
+class PagerIndexController
+{
+    protected $model
+    protected $templating;
+    protected $template;
+
+    public function __construct(
+        TraversableListInterface $model,
+        EngineInterface $templating,
+        $template = 'index.html.twig',
+        $maxPerPage = 15
+    )
+    {
+        $this->model = $model;
+        $this->templating = $templating;
+        $this->template = $template;
+        $this->maxPerPage = $maxPerPage;
+    }
+
+    public function __invoke(
+        Request $request = new Request()
+    )
+    {
+        return new Response($this->templating->render($this->template, ['model' => $this->model->traversableList([
+            'currentPage' => $request->query->getInt('page', 1), // Could be parametrized or generalized!
+            'maxPerPage' => $this->maxPerPage,
+        ])));
+    }
+}
